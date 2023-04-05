b07107a2ad852b054aa297ea5fa4317d743a4b6a
Dennisjoch <Dennisjoch@users.noreply.github.com>
2023-04-05 15:26:44 +0000

    deploy: e38a7a9ae7699fe4cc8fbdcb860d65843e0be3da

---
+++ app.js
@@ -0,0 +1,40 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoia2FsaXJvYm90IiwiYSI6ImNsZzN0Y3lrMjA4aXEzaHFlYXczbHl5c3IifQ.vCKMxqx0hgyoHzmRcpJ60w';
+
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11',
+  center: [10.451526, 51.165691],
+  zoom: 3
+});
+
+async function fetchMostPopularEmotes() {
+  const backendUrl = 'https://emotebackend.vercel.app';
+  const response = await fetch(`${backendUrl}/api/getMostPopularEmotesByCountry`);
+  return await response.json();
+}
+
+function addEmoteMarkers(emotes) {
+  emotes.forEach(emote => {
+    const countryCode = emote.countryCode;
+    const countryData = EUROPEAN_COUNTRIES.find(country => country.countryCode === countryCode);
+
+    if (countryData) {
+      const el = document.createElement('div');
+      el.className = 'marker';
+      el.textContent = emote.emote;
+      el.style.fontSize = '24px';
+
+      new mapboxgl.Marker(el)
+        .setLngLat([countryData.longitude, countryData.latitude])
+        .addTo(map);
+    }
+  });
+}
+
+async function updateMap() {
+  const mostPopularEmotes = await fetchMostPopularEmotes();
+  addEmoteMarkers(mostPopularEmotes);
+}
+
+updateMap();
+setInterval(updateMap, 300000); // Alle 5 Minuten aktualisieren
