06d52b70bf48bbf4bd7b7e4d9d5fc0a07a666259
Dennis Joch <41139171+Dennisjoch@users.noreply.github.com>
2023-04-06 13:27:49 +0200

    Add files via upload

---
+++ oceaniaCountries.js
@@ -0,0 +1,17 @@
+export const OCEANIA_COUNTRIES = [
+    { name: 'Australia', countryCode: 'AU', latitude: -25.274398, longitude: 133.775136 },
+    { name: 'Fiji', countryCode: 'FJ', latitude: -17.713371, longitude: 178.065032 },
+    { name: 'Kiribati', countryCode: 'KI', latitude: -3.370417, longitude: -168.734039 },
+    { name: 'Marshall Islands', countryCode: 'MH', latitude: 7.131474, longitude: 171.184478 },
+    { name: 'Micronesia', countryCode: 'FM', latitude: 7.425554, longitude: 150.550812 },
+    { name: 'Nauru', countryCode: 'NR', latitude: -0.522778, longitude: 166.931503 },
+    { name: 'New Zealand', countryCode: 'NZ', latitude: -40.900557, longitude: 174.885971 },
+    { name: 'Palau', countryCode: 'PW', latitude: 7.51498, longitude: 134.58252 },
+    { name: 'Papua New Guinea', countryCode: 'PG', latitude: -6.314993, longitude: 143.95555 },
+    { name: 'Samoa', countryCode: 'WS', latitude: -13.759029, longitude: -172.104629 },
+    { name: 'Solomon Islands', countryCode: 'SB', latitude: -9.64571, longitude: 160.156194 },
+    { name: 'Tonga', countryCode: 'TO', latitude: -21.178986, longitude: -175.198242 },
+    { name: 'Tuvalu', countryCode: 'TV', latitude: -7.109535, longitude: 177.64933 },
+    { name: 'Vanuatu', countryCode: 'VU', latitude: -15.376706, longitude: 166.959158 }
+  ];
+  
